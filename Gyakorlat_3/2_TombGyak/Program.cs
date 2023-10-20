26273a6a03e5f7d8dd3c645bad64a7614a8054cb
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:47:32 +0200

    Initial Feladat 2 Max Even Number

---
+++ Gyakorlat_3/2_TombGyak/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_TombGyak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = RandomArray();
+            PrintArray(array);
+            Console.WriteLine("Legnagyobb páros szám: " + MaxEvenNumber(array));
+        }
+
+        static int[] RandomArray()
+        {
+            Random r = new Random();
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++)
+                arr[i] = r.Next(1,51);
+            return arr;
+        }
+        static void PrintArray(int[] arr)
+        {
+            Console.Write("The array: ");
+            foreach (var item in arr)
+            {
+                Console.Write(item + ", ");
+            }
+            Console.WriteLine("");
+        }
+
+        static int MaxEvenNumber(int[] arr)
+        {
+            return arr.Where((a) => { return a % 2 == 0; }).Max();
+        }
+    }
+}
