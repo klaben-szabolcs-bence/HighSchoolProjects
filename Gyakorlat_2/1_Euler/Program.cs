3f2198b6696fb9c636495d80df6a5b350a4c3b3c
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:44:05 +0200

    Euler program

--- Gyakorlat_2/1_Euler/Program.cs
+++ Gyakorlat_2/1_Euler/Program.cs
@@ -10,6 +10,27 @@ namespace _1_Euler
     {
         static void Main(string[] args)
         {
+            double eulerSzam = 0;
+            int darabszam = 11;
+
+            for (int i = 0; i < darabszam; i++)
+            {
+                eulerSzam += 1.0 / Faktorialis(i);
+            }
+
+            Console.WriteLine("e = {0}", eulerSzam);
+        }
+
+        private static double Faktorialis(int szam)
+        {
+            int faktorialis = 1;
+
+            while (szam > 0)
+            {
+                faktorialis *= szam;
+                szam--;
+            }
+            return faktorialis;
         }
     }
 }
