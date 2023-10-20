6fe1c68c309056b3b72a919a500e3203170a3517
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:47:32 +0200

    Gyakorlat 2 Vége

--- Gyakorlat_3/2_TombGyak/Program.cs
+++ Gyakorlat_3/2_TombGyak/Program.cs
@@ -10,17 +10,30 @@ namespace _2_TombGyak
     {
         static void Main(string[] args)
         {
-            int[] array = RandomArray();
+            int[] array = RandomIntArray();
             PrintArray(array);
-            Console.WriteLine("Legnagyobb páros szám: " + MaxEvenNumber(array));
+            Console.WriteLine("Largest even number: " + MaxEvenNumber(array));
+            Console.WriteLine("Count of square numbers: " + SquareNumberCount(array));
+            double[] d_array = RandomDoubleArray();
+            Console.WriteLine("Geometric avarage: " + GeometricAvg(array));
+            
+            // There could be other types of array functions here, but I'm too lazy :shrug:
         }
 
-        static int[] RandomArray()
+        static int[] RandomIntArray()
         {
             Random r = new Random();
             int[] arr = new int[10];
             for (int i = 0; i < 10; i++)
                 arr[i] = r.Next(1,51);
+            return arr;
+        }
+        static double[] RandomDoubleArray()
+        {
+            Random r = new Random();
+            double[] arr = new double[10];
+            for (int i = 0; i < 10; i++)
+                arr[i] = r.Next(1, 51);
             return arr;
         }
         static void PrintArray(int[] arr)
@@ -36,6 +49,21 @@ namespace _2_TombGyak
         static int MaxEvenNumber(int[] arr)
         {
             return arr.Where((a) => { return a % 2 == 0; }).Max();
+        }
+
+        static int SquareNumberCount(int[] arr)
+        {
+            return arr.Where((a) => { return Math.Sqrt(a) == (int)Math.Sqrt(a); }).Count();
+        }
+
+        static double GeometricAvg(int[] arr)
+        {
+            double product = 1;
+            foreach (var item in arr)
+            {
+                product *= item;
+            }
+            return Math.Pow(product, 1.0 / arr.Length);
         }
     }
 }
