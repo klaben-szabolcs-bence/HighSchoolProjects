104dda17d74d3bcc9b82bb207877c5151ca73317
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:47:31 +0200

    Main Menu

--- Gyakorlat_3/1_Szamologep/Program.cs
+++ Gyakorlat_3/1_Szamologep/Program.cs
@@ -10,6 +10,38 @@ namespace _1_Szamologep
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Choose mode:\na) Seperated\nb)One-line\nc)Exit");
+            do
+            {
+                switch(Console.ReadKey().KeyChar)
+                {
+                    case 'a': Seperated(); break;
+                    case 'b': OneLine(); break;
+                    case 'c': return;
+                    default: Console.WriteLine("Invalid option!"); break;
+                }
+            } while (true);
+        }
+
+        private static void OneLine()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void IntegerScan(out int number)
+        {
+            bool success;
+            Console.WriteLine("Enter a number.");
+            do {
+                string string_value = Console.ReadLine();
+                success = Int32.TryParse(string_value, out number);
+                if (!success) Console.WriteLine("That's not a number.");
+            } while (!success);
+        }
+
+        private static void Seperated()
+        {
+
         }
     }
 }
