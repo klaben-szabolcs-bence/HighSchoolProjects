1c0b26ae6cd2457714ff03465b855945ed0c7250
Kláben Szabolcs Bence <szabolcs.bence@klaben.hu>
2023-10-20 16:50:49 +0200

    Gyakorlat 4

---
+++ Gyakorlat_04/1_Employee/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Employee
+{
+    class Employee
+    {
+        int invisibleInteger;
+        internal readonly string name = "Unnamed Employee";
+        internal long salary;
+
+        public Employee()
+        {
+        }
+
+        public Employee(string name) {
+            this.name = name;
+        }
+
+        public Employee(string name, long salary)
+        {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public void IncreaseSalary(long increment)
+        {
+            salary += increment;
+        }
+
+        public string PrintInformation()
+        {
+            return $"Name: {name}, Salary: {salary}";
+        }
+    }
+}
---
+++ Gyakorlat_04/1_Employee/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee_A = new Employee();
+
+            Console.WriteLine(employee_A.PrintInformation());
+
+            //Employee[] employees = new Employee[10];
+            //employees[0] = employee_A;
+
+            employee_A.salary = 1;
+
+            Employee employee_B = new Employee("John Smith");
+            employee_B.IncreaseSalary(10000);
+
+            Console.WriteLine(employee_B.PrintInformation());
+
+            
+        }
+    }
+}
